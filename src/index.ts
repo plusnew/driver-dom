9cbd7c56cced6f28070db676c97d33898dc1dcd1
plusgut <github@webentwickler2-0.de>
2020-01-23 06:46:26 +0100

    [lifecycle] renamed to lifecycle-component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import driver from './driver';
 import './jsx';
 
-export { default as ElementHook } from './components/ElementHook';
+export { default as ElementLifecycle } from './components/ElementLifecycle';
 export { default as Idle } from './components/Idle';
 
 export default driver;
