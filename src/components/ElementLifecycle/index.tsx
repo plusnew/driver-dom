d16757b3f19fb52a99c051ce048cd3c3e127f613
plusgut <github@webentwickler2-0.de>
2020-01-23 07:21:37 +0100

    [lifecycle] renamed to lifecycle-component

---
+++ src/components/ElementLifecycle/index.tsx
@@ -0,0 +1,53 @@
+import plusnew, { component, ComponentContainer } from '@plusnew/core/src/index';
+import Instance from '@plusnew/core/src/instances/types/Component/Instance';
+
+type props = {
+  elementDidMount?: (element: Element) => void;
+  elementWillUnmount?: (element: Element) => Promise<any> | void;
+  children: any,
+};
+
+const ElementLifecycle: ComponentContainer<props, Element, Text> = component(
+  'ElementLifecycle',
+  (Props, instance) => {
+    return <Props>{(props) => {
+      instance.elementDidMount = (element: Element) => {
+        (instance.parentInstance as Instance<props, Element, Text>).elementDidMount(element);
+        if (props.elementDidMount) {
+          props.elementDidMount(element);
+        }
+      };
+
+      instance.elementWillUnmount = (element: Element): Promise<any> | void => {
+        let parentWait: void | Promise<any> = undefined;
+        parentWait = (instance.parentInstance as Instance<props, Element, Text>).elementWillUnmount(element);
+
+        if (parentWait) {
+          return new Promise((resolve) => {
+            (parentWait as Promise<any>).then(() => {
+              const elementWillUnmount = instance.props.elementWillUnmount;
+              if (elementWillUnmount) {
+                // @FIXME the as Promise seems to be wrong, a typeguard is probably needed
+                (elementWillUnmount(element) as Promise<any>).then(() => resolve());
+              } else {
+                resolve();
+              }
+            });
+          });
+        }
+
+        const elementWillUnmount = instance.props.elementWillUnmount;
+
+        if (elementWillUnmount) {
+          return elementWillUnmount(element);
+        }
+      };
+
+      return props.children;
+    }}</Props>;
+  },
+);
+
+export default ElementLifecycle;
+
+export { props };
